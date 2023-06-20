038f475edb08b0ec05c3c556253de680e9a2aad3
dimavorobey160892 <dimavorobey160892@gmail.com>
2023-06-20 18:15:04 +0300

    add server validation

--- MyFinalProject/Models/CallRequestModel.cs
+++ MyFinalProject/Models/CallRequestModel.cs
@@ -1,8 +1,14 @@
-﻿namespace MyFinalProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFinalProject.Models
 {
     public class CallRequestModel
     {
+        [Required(ErrorMessage = "Enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter your phone number")]
+        [RegularExpression(@"^\+?3?8?(0\d{9})$", ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
     }
 }
